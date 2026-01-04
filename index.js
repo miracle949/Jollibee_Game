659b790e509e3b26301a7e49f4bc51fdd14fadd5
Rogelio Amoyan <rogelioamoyan123@gmail.com>
2026-01-04 23:54:34 +0800

    update codes

--- index.js
+++ index.js
@@ -5,8 +5,15 @@ const start_button = document.querySelector(".start-button");
 const get_order = document.querySelector(".get-order");
 const prepare_order = document.querySelector(".prepare-order");
 const cashier = document.querySelector(".cashier");
+const menu = document.querySelector(".menu");
+const title_order = document.querySelector(".title-order");
+
+
 
 start_button.addEventListener("click", () => {
+
+    document.getElementById("audio").play();
+
     body.style.backgroundImage = "none";
     body.style.backgroundColor = "#ff0000";
     start_button.style.display = "none";
@@ -34,8 +41,26 @@ get_order.addEventListener("click", () => {
     mechanics.style.display = "none";
 
     body.style.backgroundImage = "url('images/customer1.jpg')";
-    
+
     cashier.style.display = "block";
 
     get_order.style.display = "none";
-});
+}); 
+
+prepare_order.addEventListener("click", () => {
+    body.style.backgroundImage = "none";
+    body.style.backgroundColor = "transparent";
+    start_button.style.display = "none";
+
+    mechanics.style.display = "none";
+
+    body.style.backgroundImage = "url('images/orderBg2.jpg')";
+
+    cashier.style.display = "none";
+
+    get_order.style.display = "none";
+
+    menu.style.display = "flex";
+
+    title_order.style.display = "block";
+}); 
