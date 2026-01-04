41345ec7d5b55f895c0fb8daae47198fbdcef6d6
Rogelio Amoyan <rogelioamoyan123@gmail.com>
2026-01-04 16:52:49 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,41 @@
+const mechanics = document.querySelector(".mechanics");
+const body = document.querySelector(".body");
+const play_button = document.querySelector(".play-button");
+const start_button = document.querySelector(".start-button");
+const get_order = document.querySelector(".get-order");
+const prepare_order = document.querySelector(".prepare-order");
+const cashier = document.querySelector(".cashier");
+
+start_button.addEventListener("click", () => {
+    body.style.backgroundImage = "none";
+    body.style.backgroundColor = "#ff0000";
+    start_button.style.display = "none";
+
+    mechanics.style.display = "flex";
+});
+
+play_button.addEventListener("click", () => {
+    body.style.backgroundImage = "none";
+    body.style.backgroundColor = "transparent";
+    start_button.style.display = "none";
+
+    mechanics.style.display = "none";
+
+    body.style.backgroundImage = "url('images/customer1.jpg')";
+
+    get_order.style.display = "block";
+});
+
+get_order.addEventListener("click", () => {
+    body.style.backgroundImage = "none";
+    body.style.backgroundColor = "transparent";
+    start_button.style.display = "none";
+
+    mechanics.style.display = "none";
+
+    body.style.backgroundImage = "url('images/customer1.jpg')";
+    
+    cashier.style.display = "block";
+
+    get_order.style.display = "none";
+});
