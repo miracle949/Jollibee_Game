726aaa2e108abcd9a7bb7a8afadcca09ae61495e
Rogelio Amoyan <rogelioamoyan123@gmail.com>
2026-01-06 01:43:59 +0800

    update the codes

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const prepare_order = document.querySelector(".prepare-order");
 const cashier = document.querySelector(".cashier");
 const menu = document.querySelector(".menu");
 const title_order = document.querySelector(".title-order");
+
 const bucketChicken = document.querySelector(".bucketChicken");
 const Fries = document.querySelector(".Fries");
 const cokeFloat = document.querySelector(".title-order");
@@ -17,6 +18,14 @@ const Peach = document.querySelector(".Peach");
 const start_answer_group = document.querySelector(".start-answer-group");
 
 const start_answer_group2 = document.querySelector(".start-answer-group2");
+
+const start_answer_group3 = document.querySelector(".start-answer-group3");
+
+const start_answer_group4 = document.querySelector(".start-answer-group4");
+
+const start_answer_group5 = document.querySelector(".start-answer-group5");
+
+const start_answer_group6 = document.querySelector(".start-answer-group6");
 
 function formatTime(seconds) {
     return `00:${String(seconds).padStart(2, "0")}`;
@@ -66,10 +75,98 @@ function startTimer2(duration, onFinish) {
     }, 1000);
 }
 
+function startTimer3(duration, onFinish) {
+    let count = duration;
+
+    timer3.textContent = formatTime(count);
+
+    const interval = setInterval(() => {
+        count--;
+        timer3.textContent = formatTime(count);
+
+        if (count === 0) {
+            clearInterval(interval);
+            timer3.textContent = "00:00";
+
+            if (typeof onFinish === "function") {
+                onFinish();
+
+                timer3.textContent = "Start";
+            }
+        }
+    }, 1000);
+}
+
+function startTimer4(duration, onFinish) {
+    let count = duration;
+
+    timer4.textContent = formatTime(count);
+
+    const interval = setInterval(() => {
+        count--;
+        timer4.textContent = formatTime(count);
+
+        if (count === 0) {
+            clearInterval(interval);
+            timer4.textContent = "00:00";
+
+            if (typeof onFinish === "function") {
+                onFinish();
+
+                timer4.textContent = "Start";
+            }
+        }
+    }, 1000);
+}
+
+function startTimer5(duration, onFinish) {
+    let count = duration;
+
+    timer5.textContent = formatTime(count);
+
+    const interval = setInterval(() => {
+        count--;
+        timer5.textContent = formatTime(count);
+
+        if (count === 0) {
+            clearInterval(interval);
+            timer5.textContent = "00:00";
+
+            if (typeof onFinish === "function") {
+                onFinish();
+
+                timer5.textContent = "Start";
+            }
+        }
+    }, 1000);
+}
+
+function startTimer6(duration, onFinish) {
+    let count = duration;
+
+    timer6.textContent = formatTime(count);
+
+    const interval = setInterval(() => {
+        count--;
+        timer6.textContent = formatTime(count);
+
+        if (count === 0) {
+            clearInterval(interval);
+            timer6.textContent = "00:00";
+
+            if (typeof onFinish === "function") {
+                onFinish();
+
+                timer6.textContent = "Start";
+            }
+        }
+    }, 1000);
+}
+
 
 start_button.addEventListener("click", () => {
 
-    // document.getElementById("audio").play();
+    document.getElementById("audio").play();
 
     body.style.backgroundImage = "none";
     body.style.backgroundColor = "#ff0000";
@@ -145,6 +242,14 @@ bucketChicken.addEventListener("click", () => {
 
     document.querySelector(".question-fries").style.display = "none";
 
+    document.querySelector(".question-cokefloat").style.display = "none";
+
+    document.querySelector(".question-burgerFries").style.display = "none";
+
+    document.querySelector(".question-spaghetti").style.display = "none";
+
+    document.querySelector(".question-peach-mango").style.display = "none";
+
 });
 
 document.querySelector(".exit").addEventListener("click", () => {
@@ -165,6 +270,7 @@ document.querySelector(".exit").addEventListener("click", () => {
     title_order.style.display = "flex";
 
     document.querySelector(".question-bucket-chicken").style.display = "none";
+    
 });
 
 document.querySelector(".start-answer").addEventListener("click", () => {
@@ -213,6 +319,84 @@ Fries.addEventListener("click", () => {
     title_order.style.display = "flex";
 
     document.querySelector(".question-fries").style.display = "block";
+
+    document.querySelector(".question-bucket-chicken").style.display = "none";
+
+    document.querySelector(".question-cokefloat").style.display = "none";
+
+    document.querySelector(".question-burgerFries").style.display = "none";
+
+    document.querySelector(".question-spaghetti").style.display = "none";
+
+    document.querySelector(".question-peach-mango").style.display = "none";
+});
+
+document.querySelector(".exit2").addEventListener("click", () => {
+    body.style.backgroundImage = "none";
+    body.style.backgroundColor = "transparent";
+    start_button.style.display = "none";
+
+    mechanics.style.display = "none";
+
+    body.style.backgroundImage = "url('images/orderBg2.jpg')";
+
+    cashier.style.display = "none";
+
+    get_order.style.display = "none";
+
+    menu.style.display = "flex";
+
+    title_order.style.display = "flex";
+
+    document.querySelector(".question-fries").style.display = "none";
+});
+
+document.querySelector(".start-answer2").addEventListener("click", () => {
+    body.style.backgroundImage = "none";
+    body.style.backgroundColor = "transparent";
+    start_button.style.display = "none";
+
+    mechanics.style.display = "none";
+
+    body.style.backgroundImage = "url('images/orderBg2.jpg')";
+
+    cashier.style.display = "none";
+
+    get_order.style.display = "none";
+
+    menu.style.display = "flex";
+
+    title_order.style.display = "flex";
+
+    document.querySelector(".question-fries").style.display = "block";
+
+    start_answer_group2.style.marginLeft = "1rem";
+
+    startTimer2(10, () => {
+       start_answer_group2.style.marginLeft = "0";
+    });
+});
+
+// CokeFloat
+
+cokeFloat.addEventListener("click", () => {
+    body.style.backgroundImage = "none";
+    body.style.backgroundColor = "transparent";
+    start_button.style.display = "none";
+
+    mechanics.style.display = "none";
+
+    body.style.backgroundImage = "url('images/orderBg2.jpg')";
+
+    cashier.style.display = "none";
+
+    get_order.style.display = "none";
+
+    menu.style.display = "flex";
+
+    title_order.style.display = "flex";
+
+    document.querySelector(".question-cokefloat").style.display = "block";
 
     document.querySelector(".question-bucket-chicken").style.display = "none";
 });
